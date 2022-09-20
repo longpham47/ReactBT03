1a49436ede5bb7454c71afb38f71bf747400f574
Plong <phamlonganhdao@gmail.com>
2022-09-20 18:58:11 +0700

    ui

---
+++ src/ShoesShop/ShoeShop.jsx
@@ -0,0 +1,280 @@
+import React, { Component } from 'react'
+
+export default class ShoeShop extends Component {
+
+
+    arrayShoes = [
+        {
+            "id": 1,
+            "name": "Adidas Prophere",
+            "alias": "adidas-prophere",
+            "price": 350,
+            "description": "The adidas Primeknit upper wraps the foot with a supportive fit that enhances movement.\r\n\r\n",
+            "shortDescription": "The midsole contains 20% more Boost for an amplified Boost feeling.\r\n\r\n",
+            "quantity": 995,
+            "image": "http://svcy3.myclass.vn/images/adidas-prophere.png"
+        },
+        {
+            "id": 2,
+            "name": "Adidas Prophere Black ",
+            "alias": "adidas-prophere-black-white",
+            "price": 450,
+            "description": "The adidas Primeknit upper wraps the foot with a supportive fit that enhances movement.\r\n\r\n",
+            "shortDescription": "The midsole contains 20% more Boost for an amplified Boost feeling.\r\n\r\n",
+            "quantity": 990,
+            "image": "http://svcy3.myclass.vn/images/adidas-prophere-black-white.png"
+        },
+        {
+            "id": 3,
+            "name": "Adidas Prophere Customize",
+            "alias": "adidas-prophere-customize",
+            "price": 375,
+            "description": "The adidas Primeknit upper wraps the foot with a supportive fit that enhances movement.\r\n\r\n",
+            "shortDescription": "The midsole contains 20% more Boost for an amplified Boost feeling.\r\n\r\n",
+            "quantity": 415,
+            "image": "http://svcy3.myclass.vn/images/adidas-prophere-customize.png"
+        },
+        {
+            "id": 4,
+            "name": "Adidas Super Star Red",
+            "alias": "adidas-super-star-red",
+            "price": 465,
+            "description": "The adidas Primeknit upper wraps the foot with a supportive fit that enhances movement.\r\n\r\n",
+            "shortDescription": "The midsole contains 20% more Boost for an amplified Boost feeling.\r\n\r\n",
+            "quantity": 542,
+            "image": "http://svcy3.myclass.vn/images/adidas-super-star-red.png"
+        },
+        {
+            "id": 5,
+            "name": "Adidas Swift Run",
+            "alias": "adidas-swift-run",
+            "price": 550,
+            "description": "The adidas Primeknit upper wraps the foot with a supportive fit that enhances movement.\r\n\r\n",
+            "shortDescription": "The midsole contains 20% more Boost for an amplified Boost feeling.\r\n\r\n",
+            "quantity": 674,
+            "image": "http://svcy3.myclass.vn/images/adidas-swift-run.png"
+        },
+        {
+            "id": 6,
+            "name": "Adidas Tenisky Super Star",
+            "alias": "adidas-tenisky-super-star",
+            "price": 250,
+            "description": "The adidas Primeknit upper wraps the foot with a supportive fit that enhances movement.\r\n\r\n",
+            "shortDescription": "The midsole contains 20% more Boost for an amplified Boost feeling.\r\n\r\n",
+            "quantity": 456,
+            "image": "http://svcy3.myclass.vn/images/adidas-tenisky-super-star.png"
+        },
+        {
+            "id": 7,
+            "name": "Adidas Ultraboost 4",
+            "alias": "adidas-ultraboost-4",
+            "price": 450,
+            "description": "The adidas Primeknit upper wraps the foot with a supportive fit that enhances movement.\r\n\r\n",
+            "shortDescription": "The midsole contains 20% more Boost for an amplified Boost feeling.\r\n\r\n",
+            "quantity": 854,
+            "image": "http://svcy3.myclass.vn/images/adidas-ultraboost-4.png"
+        },
+        {
+            "id": 8,
+            "name": "Adidas Yeezy 350",
+            "alias": "adidas-yeezy-350",
+            "price": 750,
+            "description": "The adidas Primeknit upper wraps the foot with a supportive fit that enhances movement.\r\n\r\n",
+            "shortDescription": "The midsole contains 20% more Boost for an amplified Boost feeling.\r\n\r\n",
+            "quantity": 524,
+            "image": "http://svcy3.myclass.vn/images/adidas-yeezy-350.png"
+        },
+        {
+            "id": 9,
+            "name": "Nike Adapt BB",
+            "alias": "nike-adapt-bb",
+            "price": 630,
+            "description": "Nike shoe is the rare high-percentage shooter who's also a coach's dream on D. Designed for his unrivaled 2-way game, the PG 4 unveils a new cushioning system that's lightweight, articulated and responsive, ideal for players like PG who go hard every play.\r\n\r\n",
+            "shortDescription": "Paul George is the rare high-percentage shooter",
+            "quantity": 599,
+            "image": "http://svcy3.myclass.vn/images/nike-adapt-bb.png"
+        },
+        {
+            "id": 10,
+            "name": "Nike Air Max 97",
+            "alias": "nike-air-max-97",
+            "price": 650,
+            "description": "Nike shoe is the rare high-percentage shooter who's also a coach's dream on D. Designed for his unrivaled 2-way game, the PG 4 unveils a new cushioning system that's lightweight, articulated and responsive, ideal for players like PG who go hard every play.\r\n\r\n",
+            "shortDescription": "Paul George is the rare high-percentage shooter",
+            "quantity": 984,
+            "image": "http://svcy3.myclass.vn/images/nike-air-max-97.png"
+        },
+        {
+            "id": 11,
+            "name": "Nike Air Max 97 Blue",
+            "alias": "nike-air-max-97-blue",
+            "price": 450,
+            "description": "Nike shoe is the rare high-percentage shooter who's also a coach's dream on D. Designed for his unrivaled 2-way game, the PG 4 unveils a new cushioning system that's lightweight, articulated and responsive, ideal for players like PG who go hard every play.\r\n\r\n",
+            "shortDescription": "Paul George is the rare high-percentage shooter",
+            "quantity": 875,
+            "image": "http://svcy3.myclass.vn/images/nike-air-max-97-blue.png"
+        },
+        {
+            "id": 12,
+            "name": "Nike Air Max 270 React",
+            "alias": "nike-air-max-270-react",
+            "price": 750,
+            "description": "Nike shoe is the rare high-percentage shooter who's also a coach's dream on D. Designed for his unrivaled 2-way game, the PG 4 unveils a new cushioning system that's lightweight, articulated and responsive, ideal for players like PG who go hard every play.\r\n\r\n",
+            "shortDescription": "Paul George is the rare high-percentage shooter",
+            "quantity": 445,
+            "image": "http://svcy3.myclass.vn/images/nike-air-max-270-react.png"
+        }
+    ]
+
+
+
+    state = {
+
+        chiTiecShoes: {
+            "id": 1,
+            "name": "Adidas Prophere",
+            "alias": "adidas-prophere",
+            "price": 350,
+            "description": "The adidas Primeknit upper wraps the foot with a supportive fit that enhances movement.\r\n\r\n",
+            "shortDescription": "The midsole contains 20% more Boost for an amplified Boost feeling.\r\n\r\n",
+            "quantity": 995,
+            "image": "http://svcy3.myclass.vn/images/adidas-prophere.png"
+        }
+    }
+
+
+
+
+
+    renderDSSP = () => {
+        return this.arrayShoes.map((shoes) => {
+            return <div className="col-4 mb-2" key={`shoes-${shoes.id}`}>
+                <div className="card">
+                    <img className="card-img-top" src={shoes.image} alt="" />
+                    <div className="card-body">
+                        <h4 className="card-title">{shoes.name}</h4>
+                        <p className="card-text">{shoes.price}$</p>
+                        <button data-toggle="modal" data-target="#modelId" className='btn btn-info'>xem chi tiếc</button>
+                    </div>
+                </div>
+
+            </div>
+
+        })
+    }
+
+
+
+
+
+
+
+    render() {
+        let { chiTiecShoes } = this.state;
+
+        return (
+            <div className='container'>
+                <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+                    <a className="navbar-brand" href="#!">ShoeShop</a>
+                    <button className="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon" />
+                    </button>
+                    <div className="collapse navbar-collapse" id="collapsibleNavId">
+                        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                            <li className="nav-item active">
+                                <a className="nav-link" href="#!">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#!">shop</a>
+                            </li>
+
+                        </ul>
+
+                    </div>
+                </nav>
+                <div className="row py-5">
+                    {this.renderDSSP()}
+                </div>
+                <div>
+
+
+                    <div className="modal fade" id="modelId" tabIndex={-1} role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
+                        <div className="modal-dialog modal-lg" role="document">
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <h5 className="modal-title py-5" >Modal title</h5>
+                                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">×</span>
+                                    </button>
+                                </div>
+                                <div className="modal-body">
+                                    <table className="table">
+
+                                        <div>
+                                            <div className="row">
+                                                <div className="col-6">
+                                                    <h3>Hình Ảnh Sản Phẩm</h3>
+                                                    <img className='img-fluid' src={chiTiecShoes.image} alt="" />
+                                                </div>
+                                                <div className="col-6">
+                                                    <h2>Chi Tiếc Sản Phẩm</h2>
+
+                                                    <tbody>
+                                                        <tr>
+                                                            <td>ID</td>
+                                                            <td>{chiTiecShoes.id}</td>
+
+                                                        </tr>
+                                                        <tr>
+                                                            <td>Tên Sản Phẩm</td>
+                                                            <td>{chiTiecShoes.name}</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>Thương Hiệu</td>
+                                                            <td>{chiTiecShoes.alias}</td>
+
+                                                        </tr>
+                                                        <tr>
+                                                            <td>Giá Tiền</td>
+                                                            <td>{chiTiecShoes.price} $</td>
+
+                                                        </tr>
+                                                        <tr>
+                                                            <td>description</td>
+                                                            <td>{chiTiecShoes.description}</td>
+
+                                                        </tr>
+                                                        <tr>
+                                                            <td>shortDescription</td>
+                                                            <td>{chiTiecShoes.shortDescription}</td>
+
+                                                        </tr>
+                                                        <tr>
+                                                            <td>số lượng</td>
+                                                            <td>{chiTiecShoes.quantity}</td>
+
+                                                        </tr>
+
+
+                                                    </tbody>
+
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </table>
+
+                                </div>
+                                <div className="modal-footer">
+                                    <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        )
+    }
+}
